d315fa185aac47d46a875fbd32bbf1a6e92310f7
Vaaleri26 <93161063+Vaaleri26@users.noreply.github.com>
2021-10-27 17:00:25 +0300

    Update cheсk.php

--- cheсk.php
+++ cheсk.php
@@ -1,34 +1,59 @@
 <?php
+require_once 'connect.php';
 $login=filter_var(trim($_POST['LOGIN']),FILTER_SANITIZE_STRING);
 $email=filter_var(trim($_POST['EMAIL']),FILTER_SANITIZE_STRING);
 $password=filter_var(trim($_POST['PASSWORD']),FILTER_SANITIZE_STRING);
 
 
 
-if(trim($login)==" "){
-    echo "Укажите логин";
+if(trim($login)==''){
+  //  echo "Укажите логин";
+  $_SESSION['errorLogin']="ERROR:Укажите логин";
+  header("Location: http://lab2.ru/logout.php");
+  exit();
+    
 }
-if(trim($email)==" "){
-    echo "Укажите email";
+if(trim($email)==''){
+    $_SESSION['errorLogin']="ERROR:Укажите email";
+    header("Location: http://lab2.ru/logout.php");
+    exit();
+      
+  
 }
-if(trim($password)==" "){
-    echo "Укажите пароль";
-}
+if(trim($password)==''){
+    
+        $_SESSION['errorLogin']="ERROR:Укажите пароль";
+        header("Location: http://lab2.ru/logout.php");
+        exit();
+          
+      
+    }
+    
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
- echo "Некорректно введён email";
- exit();
+    $_SESSION['errorLogin']="ERROR:Неккортектно введен email";
+    header("Location: http://lab2.ru/logout.php");
+    exit();
 }
-else if(mb_strlen($password)<5){
-    echo "Недопустимая длина пароля";
-    exit(); 
-}
-$result=mysqli_query($mysql,"SELECT * FROM `users` WHERE 'LOGIN'=$login");
-else if(mysql_num_rows($result)>0){
-    echo"Такой логин уже существует";
+$uppercase = preg_match('@[A-Z]@', $password);
+$lowercase = preg_match('@[a-z]@', $password);
+$number    = preg_match('@[0-9]@', $password);
+
+if(!$uppercase || !$lowercase || !$number || strlen($password) < 5) {
+    $_SESSION['errorLogin']="ERROR:Пароль должен содержать латиницу верхнего и нижнего регистра,цифры,а также иметь длину не менее пяти символов";
+    header("Location: http://lab2.ru/logout.php");
     exit();
 }
 
-require_once 'connect.php';
+$result=mysqli_query($mysql,"SELECT * FROM `users` WHERE 'LOGIN'=$login");
+
+if(mysqli_num_rows($result)>0){
+    $_SESSION['errorLogin']="ERROR:Такой логин уже существует";
+    header("Location: http://lab2.ru/logout.php");
+    exit();
+}
+
+
 mysqli_query($mysql, "INSERT INTO `users` (`ID`,`LOGIN`,`EMAIL`,`PASSWORD`)VALUES(NULL,'$login','$email','$password')");
-header('Location: logout.php');
+header('Location: signup.php');
 ?>
+
