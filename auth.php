904fc0ea7ac6c14c83340285cb8cbefc13a365f2
Vaaleri26 <93161063+Vaaleri26@users.noreply.github.com>
2021-10-27 16:59:15 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -4,15 +4,20 @@ $login=filter_var(trim($_POST['LOGIN']),FILTER_SANITIZE_STRING);
 $password=filter_var(trim($_POST['PASSWORD']),FILTER_SANITIZE_STRING);
 
 require_once 'connect.php';
-$user=mysqli_query($mysql, "SELECT * FROM `users` WHERE `LOGIN`='$login' AND `PASSWORD`='$password'");
-$row=$user.fetch_assoc();
-if(count($row)==0){
-    echo "Пользователь не найден";
-    exit();
+$users=mysqli_query($mysql, "SELECT * FROM `users` WHERE `LOGIN`='$login' AND `PASSWORD`='$password'");
+
+
+if(mysqli_num_rows($user)>0){
+$users=myscli_fetch_assoc($user);
+
+$_SESSION['user'] = [
+"ID" => $user['ID'],
+"LOGIN"=>$user['LOGIN'],
+"EMAIL"=>$user['EMAIL'],
+"PASSWORD"=>$user['PASSWORD']
+];
+
 }
 
-setcookie('user',$user['EMAIL'], time()+3600); 
 
-header('Location: logout.php');
- 
-?>
+header('Location: signup.php');
